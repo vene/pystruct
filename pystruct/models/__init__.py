685d6e4ad39306de0d915a70529a0c20a48213a3
Vlad Niculae <vlad@vene.ro>
2013-06-04 14:50:46 +0300

    Add basic linear chain CRF

--- pystruct/models/__init__.py
+++ pystruct/models/__init__.py
@@ -1,6 +1,7 @@
 from .base import StructuredModel
 from .crf import CRF
 from .grid_crf import GridCRF, DirectionalGridCRF
+from .chain_crf import ChainCRF
 from .graph_crf import GraphCRF, EdgeTypeGraphCRF
 from .latent_grid_crf import LatentGridCRF, LatentDirectionalGridCRF
 from .latent_graph_crf import LatentGraphCRF
@@ -11,6 +12,6 @@ from .edge_feature_graph_crf import EdgeFeatureGraphCRF
 
 __all__ = ["StructuredModel", "CRF", "GridCRF", "GraphCRF",
            "EdgeTypeGraphCRF", "DirectionalGridCRF", "BinarySVMModel",
-           "LatentGridCRF", "LatentDirectionalGridCRF",
+           "LatentGridCRF", "LatentDirectionalGridCRF", "ChainCRF",
            "CrammerSingerSVMModel", "LatentGraphCRF", "MultiLabelModel",
            "LatentNodeCRF", "EdgeFeatureGraphCRF"]
